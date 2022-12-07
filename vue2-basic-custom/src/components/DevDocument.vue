401bc166decf344b72b6aa1b1266b98c927e6a58
Wooara <wknowpark797@gmail.com>
2022-12-07 22:19:13 +0900

    버튼 컴포넌트 속성 추가

--- vue2-basic-custom/src/components/DevDocument.vue
+++ vue2-basic-custom/src/components/DevDocument.vue
@@ -2,15 +2,58 @@
 
 <template>
     <div>
-        DevDocument
+        <h2>Components</h2>
 
-        <ButtonComp />
+        <!-- Button -->
+        <div>
+            <h3>Button Component</h3>
+
+            <div>
+                <ButtonComp>button</ButtonComp>
+                <ButtonComp :color="'red'">button</ButtonComp>
+                <ButtonComp :color="'blue'">button</ButtonComp>
+                <ButtonComp :color="'green'">button</ButtonComp>
+                <ButtonComp :color="'gray'">button</ButtonComp>
+            </div>
+            <div>
+                <ButtonComp :disabled="true">button</ButtonComp>
+                <ButtonComp :disabled="true" :color="'red'">button</ButtonComp>
+                <ButtonComp :disabled="true" :color="'blue'">button</ButtonComp>
+                <ButtonComp :disabled="true" :color="'green'">button</ButtonComp>
+                <ButtonComp :disabled="true" :color="'gray'">button</ButtonComp>
+            </div>
+            <div>
+                <ButtonComp :outlined="true">button</ButtonComp>
+                <ButtonComp :outlined="true" :color="'red'">button</ButtonComp>
+                <ButtonComp :outlined="true" :color="'blue'">button</ButtonComp>
+                <ButtonComp :outlined="true" :color="'green'">button</ButtonComp>
+                <ButtonComp :outlined="true" :color="'gray'">button</ButtonComp>
+            </div>
+            <div>
+                <ButtonComp :disabled="true" :outlined="true">button</ButtonComp>
+                <ButtonComp :disabled="true" :outlined="true" :color="'red'">button</ButtonComp>
+                <ButtonComp :disabled="true" :outlined="true" :color="'blue'">button</ButtonComp>
+                <ButtonComp :disabled="true" :outlined="true" :color="'green'">button</ButtonComp>
+                <ButtonComp :disabled="true" :outlined="true" :color="'gray'">button</ButtonComp>
+            </div>
+            <div>
+                <ButtonComp :size="'large'">button</ButtonComp>
+                <ButtonComp>button</ButtonComp>
+                <ButtonComp :size="'small'">button</ButtonComp>
+            </div>
+        </div>
+        
     </div>
 </template>
 
 <script>
     export default {
-        name: 'DevDocument'
+        name: 'DevDocument',
+        data() {
+            return {
+                
+            }
+        }
     }
 </script>
 
--- vue2-basic-custom/src/components/common/ButtonComp.vue
+++ vue2-basic-custom/src/components/common/ButtonComp.vue
@@ -1,14 +1,42 @@
 <!-- Button Component -->
 
 <template>
-    <div>
-        Button Component
-    </div>
+    <button :type="type" 
+            :disabled="disabled" 
+            :class="[size, color, outlined]" 
+            @click="onClick">
+        <slot />
+    </button>
 </template>
 
 <script>
     export default {
         name: 'ButtonComp',
+        props: {
+            type: {
+                type: String,
+                default: 'button',
+                description: 'button | reset | submit'
+            },
+            size: {
+                type: String,
+                default: 'medium',
+                description: 'large | medium | small'
+            },
+            color: {
+                type: String,
+                default: 'black',
+                description: 'black | red | blue | green | gray'
+            },
+            outlined: {
+                type: Boolean,
+                default: false
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            }
+        },
         data() {
             return {
                 
@@ -18,7 +46,9 @@
             
         },
         methods: {
-            
+            onClick() {
+                this.$emit('click');
+            }
         },
     }
 </script>
