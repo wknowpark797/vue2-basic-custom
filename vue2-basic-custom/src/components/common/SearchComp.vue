9f0814925377c406fee6e415ef705cea27d87e31
Wooara <wknowpark797@gmail.com>
2022-12-24 17:33:32 +0900

    search 컴포넌트 버그 수정

--- vue2-basic-custom/src/components/common/SearchComp.vue
+++ vue2-basic-custom/src/components/common/SearchComp.vue
@@ -11,7 +11,7 @@
                        :disabled="disabled"
                        :readonly="readonly"
                        @input="onInput"
-                       @keyup.enter="onEnter" />
+                       @enter="onEnter" />
         </div>
         <ButtonComp class="btn-search" 
                     @click="onSearch">
