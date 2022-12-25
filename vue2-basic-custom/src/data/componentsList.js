f12fbe7134e7b8e9156cea24e373dad53e1026f0
Wooara <wknowpark797@gmail.com>
2022-12-25 18:47:31 +0900

    컴포넌트 타이틀명 변경

--- vue2-basic-custom/src/data/componentsList.js
+++ vue2-basic-custom/src/data/componentsList.js
@@ -1,132 +1,132 @@
 export default [
     { 
         seq: '1',
-        name: 'Accordion Component',
+        name: '아코디언 컴포넌트',
         component: 'AccordionExamComp'
     },
     { 
         seq: '2',
-        name: 'Alert Modal Component',
+        name: '공통 메세지 모달 컴포넌트',
         component: 'AlertModalExamComp'
     },
     { 
         seq: '3',
-        name: 'Avatar Component',
+        name: '아바타 컴포넌트',
         component: 'AvatarExamComp'
     },
     { 
         seq: '4',
-        name: 'Button Component',
+        name: '버튼 컴포넌트',
         component: 'ButtonExamComp'
     },
     { 
         seq: '5',
-        name: 'Checkbox Group Component',
+        name: '체크박스 그룹 컴포넌트',
         component: 'CheckGroupExamComp'
     },
     { 
         seq: '6',
-        name: 'Checkbox Single Component',
+        name: '체크박스 단일 컴포넌트',
         component: 'CheckSingleExamComp'
     },
     { 
         seq: '7',
-        name: 'Confirm Modal Component',
+        name: '공통 메세지 확인 모달 컴포넌트',
         component: 'ConfirmModalExamComp'
     },
     { 
         seq: '8',
-        name: 'Count Component',
+        name: '카운트 컴포넌트',
         component: 'CountExamComp' 
     },
     { 
         seq: '9', 
-        name: 'Datepicker Component',
+        name: '날짜선택 컴포넌트',
         component: 'DatepickerExamComp' 
     },
     { 
         seq: '10', 
-        name: 'Dropdown Component' ,
+        name: '드롭다운 컴포넌트' ,
         component: 'DropdownExamComp'
     },
     { 
         seq: '11', 
-        name: 'File Upload Component',
+        name: '파일 업로드 컴포넌트',
         component: 'FileUploadExamComp' 
     },
     { 
         seq: '12', 
-        name: 'Input Component',
+        name: '입력 컴포넌트',
         component: 'InputExamComp' 
     },
     { 
         seq: '13', 
-        name: 'Loading Component',
+        name: '공통 로딩 컴포넌트',
         component: 'LoadingExamComp' 
     },
     { 
         seq: '14', 
-        name: 'Modal Component',
+        name: '모달 컴포넌트',
         component: 'ModalExamComp' 
     },
     { 
         seq: '15', 
-        name: 'Pagination Component',
+        name: '페이지네이션 컴포넌트',
         component: 'PaginationExamComp' 
     },
     { 
         seq: '16', 
-        name: 'Radio Component',
+        name: '라디오 컴포넌트',
         component: 'RadioExamComp' 
     },
     { 
         seq: '17', 
-        name: 'Rating Component',
+        name: '별점 컴포넌트',
         component: 'RatingExamComp' 
     },
     { 
         seq: '18', 
-        name: 'ScrollTop Component',
+        name: '스크롤탑 컴포넌트',
         component: 'ScrollTopExamComp' 
     },
     { 
         seq: '19', 
-        name: 'Search Component',
+        name: '검색 컴포넌트',
         component: 'SearchExamComp' 
     },
     { 
         seq: '20', 
-        name: 'Select Component',
+        name: '셀렉트 컴포넌트',
         component: 'SelectExamComp' 
     },
     { 
         seq: '21', 
-        name: 'Switch Component',
+        name: '스위치 컴포넌트',
         component: 'SwitchExamComp' 
     },
     { 
         seq: '22', 
-        name: 'Tabs Component',
+        name: '탭 컴포넌트',
         component: 'TabsExamComp' 
     },
     { 
         seq: '23', 
-        name: 'Tags Component',
+        name: '태그 컴포넌트',
         component: 'TagsExamComp' 
     },
     { 
         seq: '24', 
-        name: 'Textarea Component',
+        name: '여러줄 입력 컴포넌트',
         component: 'TextareaExamComp' 
     },
     { 
         seq: '25', 
-        name: 'Timepicker Component',
+        name: '시간선택 컴포넌트',
         component: 'TimepickerExamComp'
      },
      { 
         seq: '26', 
-        name: 'Toast Component',
+        name: '공통 토스트 컴포넌트',
         component: 'ToastExamComp'
      }
 ]
