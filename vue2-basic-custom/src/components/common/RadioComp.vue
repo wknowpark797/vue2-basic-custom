4c871613e11a1a4a403cee541598b6d2524ff180
Wooara <wknowpark797@gmail.com>
2022-12-31 18:35:01 +0900

    라디오 컴포넌트 정리

--- vue2-basic-custom/src/components/common/RadioComp.vue
+++ vue2-basic-custom/src/components/common/RadioComp.vue
@@ -1,18 +1,29 @@
 <!-- 
-    [ Radio Component ]
+    [ 라디오 컴포넌트 ]
  -->
 
 <template>
     <div class="radio-wrap">
         
-        <label v-for="(pick, idx) in options" 
-               :key="idx">
-            <input type="radio" 
-                   v-model="compValue"
-                   :value="pick.value"
-                   @change="onChange">
+        <label 
+            v-for="(pick, idx) in options" 
+            :key="idx">
+            <input 
+                type="radio" 
+                v-model="compValue"
+                :value="pick.value"
+                @change="onChange">
             {{ pick.label }}
         </label>
+
+        <!-- 에러 메세지 -->
+        <div
+            v-if="errorMsg" 
+            class="sub-wrap">
+            <p class="error">
+                {{ errorMsg }}
+            </p>
+        </div>
 
     </div>
 </template>
@@ -37,6 +48,10 @@
             options: {
                 type: Array,
                 default: () => []
+            },
+            errorMsg: {
+                type: String,
+                default: ''
             }
         },
         methods: {
@@ -51,6 +66,14 @@
     .radio-wrap {
         label {
             display: block;
+        }
+        .sub-wrap {
+            @include body-3;
+            padding: 0 3px;
+            margin-top: 3px;
+            .error {
+                color: $red-05;
+            }
         }
     }
 </style>
--- vue2-basic-custom/src/components/example/RadioExamComp.vue
+++ vue2-basic-custom/src/components/example/RadioExamComp.vue
@@ -1,16 +1,21 @@
 <!-- 
-    [ Radio Example Component ]
+    [ 라디오 컴포넌트 Example ]
  -->
 
 <template>
     <div class="radio-exam-wrap">
         <RadioComp 
             v-model="radioValue" 
-            :options="radioOptions" />
+            :options="radioOptions"
+            :errorMsg="radioValue ? '':'필수 선택 항목입니다.'" />
 
         <div class="value-box">
-            <p class="tit">Value</p>
-            <p class="cont">{{ radioValue }}</p>
+            <p class="tit">
+                VALUE : 
+            </p>
+            <p class="cont">
+                {{ radioValue }}
+            </p>
         </div>
     </div>
 </template>
