a15445df786dc5b93bdace3b7cfd7d7e2ca65d66
Wooara <wknowpark797@gmail.com>
2023-01-01 00:16:03 +0900

    파일 업로드 컴포넌트 수정

--- vue2-basic-custom/src/components/common/FileUploadComp.vue
+++ vue2-basic-custom/src/components/common/FileUploadComp.vue
@@ -1,14 +1,33 @@
 <!-- 
-    [ File Upload Component ]
+    [ 파일 업로드 컴포넌트 ]
  -->
 
 <template>
     <div class="file-upload-wrap">
+
         <input 
-            type="file" 
+            type="file"
+            :multiple="multiple" 
             @change="onChange">
-            
-        <p>{{ fileName }}</p>
+        
+        <!-- 파일 리스트 -->
+        <ul class="list-file">
+            <li 
+                v-for="(file, idx) in compValue"
+                :key="idx">
+                {{ file.name }}
+            </li>
+        </ul>
+
+        <!-- 에러 메세지 -->
+        <div
+            v-if="errorMsg" 
+            class="sub-wrap">
+            <p class="error">
+                {{ errorMsg }}
+            </p>
+        </div>
+
     </div>
 </template>
 
@@ -21,20 +40,26 @@
         },
         data() {
             return {
-                compValue: this.value,
-                fileName: ''
+                compValue: this.value
             }
         },
         props: {
             value: {
-                type: File,
-                default: null
+                type: FileList,
+                default: () => {}
+            },
+            multiple: {
+                type: Boolean,
+                default: true
+            },
+            errorMsg: {
+                type: String,
+                default: ''
             }
         },
         methods: {
             onChange(event) {
-                this.compValue = event.target.files[0];
-                this.fileName = this.compValue.name;
+                this.compValue = event.target.files;
                 this.$emit('change', this.compValue);
             }
         }
@@ -42,5 +67,22 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .file-upload-wrap {
+        .list-file {
+            margin-top: 10px;
+            > li {
+                &:not(:first-child) {
+                    margin-top: 5px;
+                }
+            }
+        }
+        .sub-wrap {
+            @include body-3;
+            padding: 0 3px;
+            margin-top: 3px;
+            .error {
+                color: $red-05;
+            }
+        }
+    }
 </style>
--- vue2-basic-custom/src/components/example/FileUploadExamComp.vue
+++ vue2-basic-custom/src/components/example/FileUploadExamComp.vue
@@ -1,12 +1,14 @@
 <!-- 
-    [ File Upload Example Component ]
+    [ 파일 업로드 컴포넌트 Example ]
  -->
 
 <template>
     <div class="file-upload-exam-wrap">
 
         <!-- Component -->
-        <FileUploadComp v-model="fileUploadValue" />
+        <FileUploadComp 
+            v-model="fileUploadValue"
+            :errorMsg="errorMsg" />
     
     </div>
 </template>
@@ -14,6 +16,15 @@
 <script>
     export default {
         name: 'FileUploadExamComp',
+        computed: {
+            errorMsg() {
+                if(this.fileUploadValue && this.fileUploadValue.length > 0) {
+                    return '';
+                } else {
+                    return '필수 선택 항목입니다.';
+                }
+            }
+        },
         data() {
             return {
                 fileUploadValue: null
